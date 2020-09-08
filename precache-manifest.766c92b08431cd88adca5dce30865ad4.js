18ddf592e44d2702ac7301d1e2bf700eee3f6d98
Apurba Kr. Ghosh <apurba.kumarghosh.ece22@heritageit.edu.in>
2020-09-09 03:47:48 +0530

    Updates

---
+++ precache-manifest.766c92b08431cd88adca5dce30865ad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f9744fbc3f2ae134cae1e980ca95db",
+    "url": "/Speed-Typing-App/index.html"
+  },
+  {
+    "revision": "22256fbc805e2481bf6f",
+    "url": "/Speed-Typing-App/static/css/main.c5328da5.chunk.css"
+  },
+  {
+    "revision": "df0d965f435bb7f44710",
+    "url": "/Speed-Typing-App/static/js/2.01dcb07c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Speed-Typing-App/static/js/2.01dcb07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22256fbc805e2481bf6f",
+    "url": "/Speed-Typing-App/static/js/main.5a2a2c7d.chunk.js"
+  },
+  {
+    "revision": "b751d89487238a0f2b8f",
+    "url": "/Speed-Typing-App/static/js/runtime-main.3ce5feb7.js"
+  }
+]);
